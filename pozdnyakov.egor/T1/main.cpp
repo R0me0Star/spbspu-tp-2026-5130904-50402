fafcbdf78394885d6bc3291ec6e78d1777352cac
R0me0Star <eip_spb@mail.ru>
2026-04-04 19:58:37 +0300

    copied cli-example for main

--- pozdnyakov.egor/T1/main.cpp
+++ pozdnyakov.egor/T1/main.cpp
@@ -1,2 +1,29 @@
+#include <iostream>
+
 int main()
-{}
+{
+
+  std::vector< std::string > db;
+
+  using cmd_t = void (*)(std::istream &, std::ostream &, std::vector< std::string > &);
+  std::unordered_map< std::string, cmd_t > cmds;
+  cmds["add"] = add_string;
+  cmds["show-last"] = show_last;
+  cmds["hello"] = hi;
+  cmds["noop"] = noop;
+
+  std::string cmd;
+  while (std::cin >> cmd) {
+    try {
+      cmds.at(cmd)(std::cin, std::cout, db);
+    } catch (const std::out_of_range &) {
+      std::cout << "<INVALID COMMAND>\n";
+      auto toignore = std::numeric_limits< std::streamsize >::max();
+      std::cin.ignore(toignore, '\n');
+    } catch (const std::logic_error &e) {
+      std::cout << "<INVALID COMMAND: " << e.what() << ">\n";
+    }
+  }
+
+  return 0;
+}
